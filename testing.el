924e8852a9f9f72651ec7a9a4df8cb867638ed97
Johan Sternerup <johan.sternerup@gmail.com>
2024-03-29 21:30:05 +0100

    Wrote a function to create a new process-environment

--- testing.el
+++ testing.el
@@ -13,6 +13,39 @@ like ((\"KEY1\" . \"value1\") (\"KEY2\" . \"value2\"))"
         (push `(,(match-string 1) . ,(match-string 2)) alist))
       alist)))
 
+(defun env-get-process-environment-from-alist(alist &optional include-keys)
+  "Given an alist with KEY/VALUE elements, combine the existing
+process-environment with the alist so that the alist values adds
+or replaces lines like 'KEY=VALUE' that can later on be used to
+set the process-environment.
+
+'include-keys' is a list of strings that specifies which keys
+from the alist that should be used. may be used to filter only
+specific keys. nil means every key is accepted."
+  (interactive "xgimme an alist: ")
+  (let ((env (copy-sequence process-environment)))
+    (dolist (item alist)
+      (let ((key (car item))
+            (value (cdr item)))
+        (when (or (not include-keys) (member key include-keys))
+          (progn
+            (setq env
+                  (cl-remove-if
+                   (lambda(x) (string-prefix-p (concat key "=") x)) env))
+            (push (concat key "=" value) env)))))
+    env))
+
+(message "%s" (env-get-process-environment-from-alist
+               '(("WSLENV" . "apa")
+                 ("EDITOR" . "emacs")
+                 ("TMUX_PANE" . "jupp")
+                 )
+               '("WSLENV" "EDITOR")
+               )
+         )
+
+(setq my-list (cl-remove-if (lambda (x) (and (stringp x) (string-prefix-p prefix-to-remove x))) my-list))
+(message "%s" (env-get-process-environment-from-alist '((2 . 3))))
 (message "%s" (env-alist-from-key-value-file "key_value.txt"))
 
 (defun plist-from-key-value-file (file)
